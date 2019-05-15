e7fa8e21a2fe276b23ca20c77b79182d748e2640
Martin9527 <mjmn4118@game.ntes>
2019-05-15 11:27:02 +0800

    max area container

---
+++ maxArea.py
@@ -0,0 +1,40 @@
+class Solution(object):
+	def maxArea(self, height):
+		"""
+		:type height: List[int]
+		:rtype: int
+		"""
+		if not height or len(height) < 2:
+			return 0
+		maxArea = 0
+		for i in range(len(height)):
+			for j in range(i,len(height)):
+				h = min(height[i],height[j])
+				maxArea = max(maxArea,(j-i)*h)
+		return maxArea
+
+	def maxArea2(self,height):
+
+		if not height or len(height) < 2:
+			return 0
+		maxArea = 0
+		length = len(height)
+		i = 0
+		j = length - 1
+		while i < length and j >= i:
+			h = min(height[i],height[j])
+			maxArea = max(maxArea,(j - i)*h)
+			if height[i] < height[j]:
+				i += 1
+			else:
+				j -= 1
+		return maxArea
+		
+
+if __name__ == '__main__':
+	so = Solution()
+	maxArea = so.maxArea([1,8,6,2,5,4,8,3,7])
+	
+	
+
+
